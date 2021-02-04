41c83317971476623befe8b37c13d43dcc98d0c2
lomnom <57130071+lomnom@users.noreply.github.com>
2021-02-04 20:24:08 +0800

    Add files via upload
    C IS TERRIBLE AND IM NOT GONNA OPTIMISE IT

---
+++ NUMBER.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// Main Method 
+int main() { 
+      
+    int NUMBER=0;
+    while (1)
+    {
+        printf("%d\n",NUMBER);
+        printf("-%d\n",NUMBER);
+        NUMBER=NUMBER+1;
+    }
+    return 0;
+}
