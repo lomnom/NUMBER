34f61d3a88eb353ac1d76be6c36e3f148492cb55
lomnom <57130071+lomnom@users.noreply.github.com>
2021-02-04 20:32:50 +0800

    Add files via upload
    JAVAAAA

---
+++ NUMBER.java
@@ -0,0 +1,12 @@
+public class NUMBER {
+    public static void main(String[] args) {
+    	long NUMBER=0;
+    	String NUM;
+        while (true) {
+        	NUM=String.valueOf(NUMBER);
+            System.out.println(NUM);
+            System.out.println("-"+NUM);
+            NUMBER=NUMBER+1;
+        }
+    }
+}
