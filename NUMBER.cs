212bc1e50ec420d4d91737a3fe7383d3d2e402e6
lomnom <57130071+lomnom@users.noreply.github.com>
2021-02-04 19:33:58 +0800

    Add files via upload

---
+++ NUMBER.cs
@@ -0,0 +1,24 @@
+
+// C# program to print Hello World! 
+using System; 
+  
+// namespace declaration 
+namespace NUMBER { 
+      
+    // Class declaration 
+    class NUMBERCOUNTER { 
+          
+        // Main Method 
+        static void Main(string[] args) { 
+              
+            int NUMBER=0;
+            while (true)
+            {
+                Console.WriteLine(NUMBER);
+                Console.WriteLine("-"+NUMBER.ToString());
+                NUMBER+=1;
+            }
+               
+        } 
+    } 
+} 
